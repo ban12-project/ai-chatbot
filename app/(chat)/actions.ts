d9fa2fc2c42dca4113b9f4d8422dc72e2aba65c8
Coda <o4oban@gmail.com>
2025-01-29 09:53:01 +0800

    Squashed commit of the following:
    commit 5bb62f1b14580d9c9f80fac862dfd55932e9b743
    Author: Jeremy <git@jrmyphlmn.com>
    Date:   Tue Jan 28 19:45:03 2025 +0530
    
        fix: edit messages with votes (#737)
    
    commit 2d47ffbd778462e27cca9257c2b37b01e1580857
    Author: Jeremy <git@jrmyphlmn.com>
    Date:   Tue Jan 28 18:51:04 2025 +0530
    
        refactor: use generateId() for id creation (#736)
    
    commit 085f4a8ac4f2d4df8b133ddfb718629daf732fbd
    Author: Jeremy <git@jrmyphlmn.com>
    Date:   Mon Jan 27 19:23:16 2025 +0530
    
        fix: restore code block loading states (#728)
    
    commit 38527ff92e4aecd1b2400b4c7f66372a753f58b2
    Author: Jeremy <git@jrmyphlmn.com>
    Date:   Mon Jan 27 14:19:47 2025 +0530
    
        feat: modularize block system (#718)

--- app/(chat)/actions.ts
+++ app/(chat)/actions.ts
@@ -1,6 +1,6 @@
 'use server';
 
-import { type CoreUserMessage, generateText } from 'ai';
+import { type CoreUserMessage, generateText, Message } from 'ai';
 import { cookies } from 'next/headers';
 import webpush, { type PushSubscription } from 'web-push';
 
@@ -25,7 +25,7 @@ export async function saveModelId(model: string) {
 export async function generateTitleFromUserMessage({
   message,
 }: {
-  message: CoreUserMessage;
+  message: Message;
 }) {
   const { text: title } = await generateText({
     model: customModel(DEFAULT_MODEL_NAME),

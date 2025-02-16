8c6aae1464b332841d08f9d0195dbf20429c38c3
Coda <o4oban@gmail.com>
2025-02-16 11:27:30 +0800

    更新模型，删除不使用的依赖

--- lib/ai/models.ts
+++ lib/ai/models.ts
@@ -1,4 +1,3 @@
-import { openai } from '@ai-sdk/openai';
 import { google } from '@ai-sdk/google';
 import { fireworks } from '@ai-sdk/fireworks';
 import {
@@ -11,11 +10,10 @@ export const DEFAULT_CHAT_MODEL: string = 'gemini-2.0-flash';
 
 export const myProvider = customProvider({
   languageModels: {
+    'gemini-2.0-pro-exp-02-05': google('gemini-2.0-pro-exp-02-05'),
     'gemini-2.0-flash-thinking-exp': google('gemini-2.0-flash-thinking-exp'),
     'gemini-2.0-flash': google('gemini-2.0-flash'),
     'gemini-exp-1206': google('gemini-exp-1206'),
-    'gemini-1.5-flash': google('gemini-1.5-flash'),
-    'gemini-1.5-pro': google('gemini-1.5-pro'),
     'deepseek-chat': fireworks('accounts/fireworks/models/deepseek-v3'),
     'chat-model-reasoning': wrapLanguageModel({
       model: fireworks('accounts/fireworks/models/deepseek-r1'),
@@ -38,6 +36,11 @@ interface ChatModel {
 
 export const chatModels: Array<ChatModel> = [
   {
+    id: 'gemini-2.0-pro-exp-02-05',
+    name: 'Gemini 2.0 Pro Exp 02-05',
+    description: 'Improved quality, especially for world knowledge, code, and long context',
+  },
+  {
     id: 'gemini-2.0-flash-thinking-exp',
     name: 'Gemini 2.0 Flash Thinking Exp',
     description: 'Reasoning for complex problems, features a new Thinking mode',
@@ -52,17 +55,6 @@ export const chatModels: Array<ChatModel> = [
     id: 'gemini-exp-1206',
     name: 'Gemini Exp 1206',
     description: 'Quality improvements, celebrate 1 year of Gemini'
-  },
-  {
-    id: 'gemini-1.5-flash',
-    name: 'Gemini 1.5 Flash',
-    description:
-      'Fast and versatile performance across a diverse variety of tasks',
-  },
-  {
-    id: 'gemini-1.5-pro',
-    name: 'Gemini 1.5 Pro',
-    description: 'Complex reasoning tasks requiring more intelligence',
   },
   {
     id: 'deepseek-chat',
